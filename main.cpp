90df6a43f386291a1788dc40e4466454376963d0
nimsolated <armindoosti03@gmail.com>
2024-11-20 10:03:51 -0800

    vm and gameWindow

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
+
+using namespace std;
+using namespace sf;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    unsigned int vmWidth = VideoMode::getDesktopMode().width / 2;
+    unsigned int vmHeight = VideoMode::getDesktopMode().height / 2;
+
+    VideoMode vm(vmWidth, vmHeight);
+    RenderWindow gameWindow(vm, "MandelbrotSet!!", Style::Default);
+
+    return 0;
 }
