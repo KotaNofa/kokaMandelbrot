f18bc011e5b9a298463638e019ba828e7f4e97d9
nimsolated <armindoosti03@gmail.com>
2024-11-23 13:18:33 -0800

    comments in main.cpp

--- main.cpp
+++ main.cpp
@@ -5,17 +5,36 @@ using namespace sf;
 
 int main()
 {
+    // Setting window size
     unsigned int vmWidth = VideoMode::getDesktopMode().width / 2;
     unsigned int vmHeight = VideoMode::getDesktopMode().height / 2;
 
+    // Initializing vm and render window
     VideoMode vm(vmWidth, vmHeight);
     RenderWindow gameWindow(vm, "MandelbrotSet!!", Style::Default);
 
+    // Loop while window open
     while (gameWindow.isOpen()) {
 
         gameWindow.clear(Color::Green);
 
+        /*******************
+            HANDLE INPUT
+        *******************/
 
+        // TODO: handling input
+
+        /*************
+            UPDATE
+        *************/
+
+        // TODO: update loop
+
+        /************
+            DRAW
+        ************/
+
+        // TODO: rest of the drawing
 
         gameWindow.display();
     }
